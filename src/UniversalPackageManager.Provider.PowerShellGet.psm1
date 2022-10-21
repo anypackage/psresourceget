aecb89887dcc4cfbf814174fcceeb967569ec2e0
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2022-10-20 22:53:21 -0500

    Add copyright notice

--- src/UniversalPackageManager.Provider.PowerShellGet.psm1
+++ src/UniversalPackageManager.Provider.PowerShellGet.psm1
@@ -1,3 +1,7 @@
+# Copyright (c) Thomas Nieto - All Rights Reserved
+# You may use, distribute and modify this code under the
+# terms of the MIT license.
+
 using module UniversalPackageManager
 using module PowerShellGet
 
