a92ff536d20df371cf2ffd3c0ccd452c44c904e4
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2023-02-10 13:57:10 -0600

    Add Set-PackageSource tests

--- test/Set-PackageSource.Tests.ps1
+++ test/Set-PackageSource.Tests.ps1
@@ -1,19 +1,92 @@
 #requires -modules AnyPackage.PowerShellGet
 
 Describe Set-PackageSource {
-    Context 'with -Uri parameter' {
+    BeforeAll {
+        $path = Get-PSDrive TestDrive | Select-Object -ExpandProperty Root
+        New-Item -Path $path\repo -ItemType Directory
+        Register-PSResourceRepository -Name Test -Uri $path\repo
+    }
 
+    AfterAll {
+        Unregister-PSResourceRepository -Name Test
+    }
+    
+    Context 'with -Uri parameter' {
+        It 'should change uri' {
+            $path = Get-PSDrive TestDrive | Select-Object -ExpandProperty Root
+
+            $source = Set-PackageSource -Name Test -Location $path -PassThru 
+
+            $source | Should -Not -BeNullOrEmpty
+            $source.Location | Should -Be ([uri]$path)
+        }
     }
 
     Context 'with -CredentialInfo parameter' {
+        It 'should register with vault <VaultName> and secret <SecretName>' -TestCases @{ VaultName = 'Test'; SecretName = 'shhh' } -Skip {
 
+            $registerPackageSourceParams = @{
+                Name = 'Test'
+                Provider = 'PowerShellGet'
+                PassThru = $true
+                CredentialInfo = $_
+            }
+
+            $source = Set-PackageSource @registerPackageSourceParams
+
+            $source | Should -Not -BeNullOrEmpty
+            $source.Metadata['CredentialInfo']['VaultName'] | Should -Be $VaultName
+            $source.Metadata['CredentialInfo']['SecretName'] | Should -Be $SecretName
+        }
     }
 
     Context 'with -Trusted parameter' {
-        
+        It 'should be trusted' {
+            $registerPackageSourceParams = @{
+                Name = 'Test'
+                Trusted = $true
+                PassThru = $true
+            }
+
+            $source = Set-PackageSource @registerPackageSourceParams
+
+            $source | Should -Not -BeNullOrEmpty
+            $source.Trusted | Should -BeTrue
+        }
+
+        It 'should not be trusted' {
+            $registerPackageSourceParams = @{
+                Name = 'Test'
+                Trusted = $false
+                PassThru = $true
+            }
+
+            $source = Set-PackageSource @registerPackageSourceParams
+
+            $source | Should -Not -BeNullOrEmpty
+            $source.Trusted | Should -BeFalse
+        }
     }
 
     Context 'with pipeline' {
+        AfterEach {
+            Set-PSResourceRepository -Name Test -Trusted:$false
+        }
         
+        It 'should accept values from Get-PackageSource' -TestCases 'Test' {
+            $source = Get-PackageSource -Name $_ |
+            Set-PackageSource -Trusted -PassThru
+
+            $source | Should -Not -BeNullOrEmpty
+            $source.Trusted | Should -BeTrue
+        }
+
+        It 'should accept values from string' -TestCases 'Test' {
+            $source = $_ |
+            Set-PackageSource -Trusted -PassThru
+
+            $source | Should -Not -BeNullOrEmpty
+            $source.Trusted | Should -BeTrue
+        }
     }
 }
