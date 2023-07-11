7f2404a0d403f6b82ef9f7230b85f85e7308d419
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2023-07-10 19:13:41 -0500

    Test

--- test/Save-Package.Tests.ps1
+++ test/Save-Package.Tests.ps1
@@ -52,10 +52,10 @@ Describe Save-Package {
     }
 
     Context 'with -Source parameter' {
-        BeforeAll {
+        BeforeEach {
             $path = Get-PSDrive TestDrive | Select-Object -ExpandProperty Root
             New-Item -Path $path/repo -ItemType Directory
-            Register-PSResourceRepository -Name Test -Uri $path/repo -Trusted
+            Register-PSResourceRepository -Name Test -Uri $path/repo -Trusted -Force
             Save-PSResource -Name PSWindowsUpdate, SNMP -Path $path/repo -TrustRepository -AsNupkg
         }
 
@@ -68,7 +68,7 @@ Describe Save-Package {
             $savePackageParams = @{
                 Name = $Name
                 Source = $Source
-                Path = (Get-PSDrive -Name Temp | Select-Object -ExpandProperty Root)
+                Path = (Get-PSDrive -Name TestDrive | Select-Object -ExpandProperty Root)
                 PassThru = $true
                 TrustSource = $true
             }
